01c0a219b05ead9e25ede9b137b3779bd36d67d1
Rushaad Mistry <rushaad@berkeley.edu>
2025-11-18 23:50:51 -0800

    Increase top padding significantly to prevent nav bar overlap

--- app/layout.tsx
+++ app/layout.tsx
@@ -39,7 +39,7 @@ export default function RootLayout({
       >
         <ThemeProvider>
           <Navigation />
-          <div className="pt-24">
+          <div className="pt-32">
             {children}
           </div>
           <EasterEggSection />
